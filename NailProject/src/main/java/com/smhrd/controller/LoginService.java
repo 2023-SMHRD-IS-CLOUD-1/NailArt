9ffd513de3860ce97c9d965cc1cfa2815d09e126
SMHRD <SMHRD@121.179.83.100>
2023-11-21 17:39:05 +0900

    1234

---
+++ NailProject/src/main/java/com/smhrd/controller/LoginService.java
@@ -0,0 +1,36 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.smhrd.model.DAO;
+import com.smhrd.model.MemberVO;
+
+public class LoginService implements Command{
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String email = request.getParameter("email");
+		String pw = request.getParameter("pw");
+		
+		MemberVO vo = new MemberVO();
+		vo.setEmail(email);
+		vo.setPw(pw);
+
+		DAO dao = new DAO();
+
+		MemberVO mvo = dao.login(vo);
+
+		if (mvo != null) {
+			HttpSession session = request.getSession();
+			session.setAttribute("result", mvo);
+
+		}
+
+		return "redirect:/Gomain.do";
+	}
+}
---
+++ NailProject/src/main/java/com/smhrd/controller/SelectAllService.java
@@ -0,0 +1,29 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.smhrd.model.DAO;
+import com.smhrd.model.MemberVO;
+
+
+public class SelectAllService implements Command{
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+
+		DAO dao = new DAO();
+		List<Object> list = dao.selectAll();
+
+
+		request.setAttribute("MemberList", list);
+
+		return "select";
+
+	}
+}
