a008d10708910ae42f22f12c6332206884e8287f
smhrd <smhrd@121.147.185.64>
2023-12-06 03:42:34 +0900

    12/6 03:42

--- NailProject/src/main/java/com/smhrd/controller/UpdateService.java
+++ NailProject/src/main/java/com/smhrd/controller/UpdateService.java
@@ -10,38 +10,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.smhrd.model.DAO;
-import com.smhrd.model.MemberVO;
 
 public class UpdateService implements Command{
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
 		HttpSession session = request.getSession();
-		MemberVO mvo = (MemberVO) session.getAttribute("result");
 		
-		String mem_id = mvo.getMemId();
-		String mem_email = request.getParameter("email");
-		String mem_name = request.getParameter("name");
-		String mem_pw = request.getParameter("password");
-		String mem_tel = request.getParameter("phone");
-		String mem_address1 = request.getParameter("address1");
-		String mem_address2 = request.getParameter("address2");
+		// String email = ((MemberVO)session.getAttribute("result")).getEmail();
+		// String pw = request.getParameter("pw");
+		// String tel = request.getParameter("tel");
+		// String address = request.getParameter("address");
+		
+		// MemberVO vo = new MemberVO();
+		//vo.setEmail(email);
+		//vo.setPw(pw);
+		//vo.setTel(tel);
+		//vo.setAddress(address);
 
-		MemberVO vo = new MemberVO();
-		vo.setMemEmail(mem_email);
-		vo.setMemPw(mem_pw);
-		vo.setMemPhone(mem_tel);
-		vo.setMemAddr1(mem_address1);
-		vo.setMemAddr2(mem_address2);
-		vo.setMemName(mem_name);
-		vo.setMemId(mem_id);
-		DAO dao = new DAO();
-		int row = dao.update(vo);
+		//DAO dao = new DAO();
+		
+		//int row = dao.update(vo);
 
-		if(row > 0) {
-		 session.setAttribute("result", vo);
-		}
-		return "redirect:/Gomypage.do";
+		// if(row > 0) {
+		// 	session.setAttribute("result", vo);
+			
+		// }
+		return "redirect:/Gomain.do";
+		
+
 	}
 }
